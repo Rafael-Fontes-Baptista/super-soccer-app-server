7da57ef942d225abb469e943d054580459b5e361
Rafael-Fontes-Baptista <rafaelfontesb@gmail.com>
2021-06-20 23:47:18 +0200

    Add on PATCH request update stars and status

--- routers/users.js
+++ routers/users.js
@@ -42,19 +42,29 @@ router.patch("/:id", async (req, res, next) => {
   try {
     const userToUpdate = await User.findByPk(req.params.id)
 
+    console.log(req.body)
+
     if (!userToUpdate) {
       return res.status(404).send("user doesn't exist")
     }
+    if (req.body.status === false || req.body.status === true) {
+      await userToUpdate.update(req.body)
+    } else if (req.body.stars) {
+      if (req.body.stars >= 1 && req.body.stars <= 5) {
+        await userToUpdate.update(req.body)
+      } else {
+        return res.status(404).send("Stars range from 1 to 5")
+      }
+    } else {
+      const { full_name, email, password, avatar_url } = req.body
+      if (!full_name || !email || !password) {
+        return res
+          .status(400)
+          .send({ message: "A user must have a full name, email and password" })
+      }
 
-    const { full_name, email, password, avatar_url } = req.body
-
-    if (!full_name || !email || !password) {
-      return res
-        .status(400)
-        .send({ message: "A user must have a full name, email and password" })
+      await userToUpdate.update({ full_name, email, password, avatar_url })
     }
-
-    await userToUpdate.update({ full_name, email, password, avatar_url })
 
     return res.status(200).send({ userToUpdate })
   } catch (e) {
