6f64062c1483a9a00fdbe380b7c4f153313f1e92
Rafael-Fontes-Baptista <rafaelfontesb@gmail.com>
2021-06-20 23:03:24 +0200

    Add endpoint for PATCH request (edit user/profile)

--- routers/users.js
+++ routers/users.js
@@ -38,4 +38,28 @@ router.post("/", async (req, res, next) => {
   }
 })
 
+router.patch("/:id", async (req, res, next) => {
+  try {
+    const userToUpdate = await User.findByPk(req.params.id)
+
+    if (!userToUpdate) {
+      return res.status(404).send("user doesn't exist")
+    }
+
+    const { full_name, email, password, avatar_url } = req.body
+
+    if (!full_name || !email || !password) {
+      return res
+        .status(400)
+        .send({ message: "A user must have a full name, email and password" })
+    }
+
+    await userToUpdate.update({ full_name, email, password, avatar_url })
+
+    return res.status(200).send({ userToUpdate })
+  } catch (e) {
+    next(e)
+  }
+})
+
 module.exports = router
