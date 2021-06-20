4ab5bac8c5173b2e9781c27a1d9b92fb115299a4
Rafael-Fontes-Baptista <rafaelfontesb@gmail.com>
2021-06-20 20:01:24 +0200

    Create model, migration and seed for teams

---
+++ models/team.js
@@ -0,0 +1,38 @@
+"use strict"
+const { Model } = require("sequelize")
+module.exports = (sequelize, DataTypes) => {
+  class team extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  team.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      abrev: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      color: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+    },
+    {
+      sequelize,
+      modelName: "team",
+    }
+  )
+  return team
+}
