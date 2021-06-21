b66c95bd3239ed7330bf5013bd5c69421010d574
Rafael-Fontes-Baptista <rafaelfontesb@gmail.com>
2021-06-21 11:55:34 +0200

    Add endpoint for PATCH request (edit info and status)

--- routers/tournaments.js
+++ routers/tournaments.js
@@ -39,4 +39,35 @@ router.post("/", async (req, res, next) => {
   }
 })
 
+router.patch("/:id", async (req, res, next) => {
+  try {
+    const tournamentToUpdate = await Tournament.findByPk(req.params.id)
+
+    if (!tournamentToUpdate) {
+      return res.status(404).send("tournament doesn't exist")
+    }
+
+    if (req.body.status) {
+      await tournamentToUpdate.update(req.body)
+    } else {
+      const { name, date, time, local } = req.body
+
+      if (!name || !date || !time) {
+        return res
+          .status(400)
+          .send({ message: "A tournament must have a name, date and time" })
+      }
+
+      if (tournamentToUpdate.status !== "open") {
+        return res.status(404).send("this tournament is not open")
+      }
+      await tournamentToUpdate.update({ name, date, time, local })
+    }
+
+    return res.status(200).send({ tournamentToUpdate })
+  } catch (e) {
+    next(e)
+  }
+})
+
 module.exports = router
