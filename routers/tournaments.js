174464e8da27ea55ed0b8c47c67085bfbd3b7f55
Rafael-Fontes-Baptista <rafaelfontesb@gmail.com>
2021-06-21 09:55:26 +0200

    Add endpoint for POST request (create tournament)

--- routers/tournaments.js
+++ routers/tournaments.js
@@ -15,4 +15,28 @@ router.get("/", async (req, res, next) => {
   }
 })
 
+router.post("/", async (req, res, next) => {
+  try {
+    const { name, date, time, local } = req.body
+
+    if (!name || !date || !time) {
+      return res
+        .status(400)
+        .send({ message: "A tournament must have a name, date and time" })
+    }
+    const newTournament = await Tournament.create({
+      name,
+      date,
+      time,
+      local,
+    })
+
+    return res
+      .status(201)
+      .send({ message: "tournament created", newTournament })
+  } catch (e) {
+    next(e)
+  }
+})
+
 module.exports = router
